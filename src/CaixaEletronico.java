8bc03321d28f96dcb118bd874edda90aeb5e926c
Livia <liviaribeirodasilva168@gmail.com>
2023-10-18 23:51:29 -0300

    Condicional composta

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,7 +1,7 @@
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 8.0;
 
         if(valorSolicitado < saldo)
         saldo = saldo - valorSolicitado;
