eae24013f19f6ebef4840b87f98f0bd7a17dfb26
Livia <liviaribeirodasilva168@gmail.com>
2023-10-20 02:30:56 -0300

    for

---
+++ src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor {
+    public static void main(String[] args){
+        for(int carnerinhos = 1; carnerinhos <=20; carnerinhos ++){
+            System.out.println("Contando Carnerinhos" + carnerinhos);
+        }
+    }
+}
