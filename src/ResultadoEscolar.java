5cde6003e2b02611cb6613253a199aab6e535173
Livia <liviaribeirodasilva168@gmail.com>
2023-10-20 01:40:01 -0300

    Condicional encadeada

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -3,9 +3,12 @@ public class ResultadoEscolar {
     int nota = 7;
 
     if(nota >= 7)
-    System.out.println("Aprovado");
+     System.out.println("Aprovado");
+
+     else if(nota >=5 && nota <7)//true ou false
+     System.out.println("Prova Recuperação");
 
     else
-    System.out.println("Reprovado");
+     System.out.println("Reprovado");
    } 
 }
